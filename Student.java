8e4ef324b4ec8bbcbda689cf6451b63b01e56092
Sakib Shahriyar <74870232+sakibshahriyar@users.noreply.github.com>
2023-12-12 10:23:13 +0600

    Phase 1 before 12/12/2023 10:30 AM

--- Student.java
+++ Student.java
@@ -1,3 +1,5 @@
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 import javax.validation.constraints.*;
 import java.util.Date;
@@ -36,6 +38,74 @@ public class Student {
     @Size(max = 100)
     private String country = "Bangladeshi";
 
-    // getters and setters
+    // Constructors, getters, and setters
 
+    public Student() {
+    }
+
+    public Student(String name, String email, Date dateOfBirth, Gender gender, String quota, String country) {
+        this.name = name;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.quota = quota;
+        this.country = country;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getQuota() {
+        return quota;
+    }
+
+    public void setQuota(String quota) {
+        this.quota = quota;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
